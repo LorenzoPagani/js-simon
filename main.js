518710a23d6533684c46f91840f8508fcd697dfa
Lorenzo Pagani <larrypg2@gmail.com>
2023-11-09 16:07:24 +0100

    build: simon 1.0

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 const numberBox = document.getElementById('numContainer')
 let numeriCasuali = []
 let numeriInseriti = []
+let numeriIndovinati = []
+let result;
 
-setTimeout(function() {
-    inputNumbers(numeriInseriti)  
-}, 11000);
 setTimeout(function () {
   numberBox.style.display = 'none'
-}, 10000)
-getRandom(numeriCasuali, numberBox);
+}, 3000)
+setTimeout(function () {
+  inputNumbers(numeriInseriti)
+  numeriIndovinati = []
+  for (let i = 0; i < numeriInseriti.length; i++) {
+    if (numeriInseriti[i] == numeriCasuali[i]) {
+      numeriIndovinati.push(i + 1)
+    }
+  }
+  result = alert(
+    `hai azzeccato ${numeriIndovinati.length} numeri. ti sei ricordato le posizioni ${numeriIndovinati}`
+  )
+}, 4000)
 
-
-
+getRandom(numeriCasuali, numberBox)
 
 function inputNumbers (array) {
   for (let i = 0; i < 5; i++) {
     const inputNum = parseInt(prompt('inserisci i numeri in fila'))
-     array.push(inputNum)
+    array.push(inputNum)
   }
 }
 
@@ -25,7 +34,7 @@ function getRandom (array, print) {
     const random = getRndInteger(1, 9)
     array.push(random)
   }
-   (print.innerText = numeriCasuali)
+  print.innerText = numeriCasuali
 }
 
 function getRndInteger (min, max) {
