c155eaaecc8384dee0c2f20c00ff9fedc1b82066
Lorenzo Pagani <larrypg2@gmail.com>
2023-11-09 16:28:20 +0100

    refactor: adds functions to optimize code.

--- main.js
+++ main.js
@@ -1,42 +1,58 @@
-const numberBox = document.getElementById('numContainer')
-let numeriCasuali = []
-let numeriInseriti = []
-let numeriIndovinati = []
-let result;
+const numberBox = document.getElementById('numContainer');
+const numeriCasuali = [];
+getRandom(numeriCasuali);
+setTimeout(function () {
+  numberBox.style.display = 'none';
+}, 3000);
 
 setTimeout(function () {
-  numberBox.style.display = 'none'
-}, 3000)
-setTimeout(function () {
-  inputNumbers(numeriInseriti)
-  numeriIndovinati = []
-  for (let i = 0; i < numeriInseriti.length; i++) {
-    if (numeriInseriti[i] == numeriCasuali[i]) {
-      numeriIndovinati.push(i + 1)
-    }
+  let numeriInseriti = inputNumbers();
+  let numeriIndovinati = checkNumbers(numeriInseriti, numeriCasuali);
+  
+  if (numeriIndovinati.length == 0) {
+    alert("non ne hai preso manco uno")
   }
-  result = alert(
-    `hai azzeccato ${numeriIndovinati.length} numeri. ti sei ricordato le posizioni ${numeriIndovinati}`
-  )
-}, 4000)
+  else if (numeriIndovinati.length == numeriInseriti.length) {
+    alert("Li hai indovinati tutti, bravo!")
+  }
+  else if (numeriIndovinati.length == 1) {
+    alert(`ne hai indovinato solo 1. ti sei ricordato il numero ${numeriIndovinati}`)
+  }
+  else {
+      alert(
+        `hai azzeccato ${numeriIndovinati.length} numeri. ti sei ricordato le posizioni ${numeriIndovinati}`
+      );
+  }
+}, 4000);
 
-getRandom(numeriCasuali, numberBox)
-
-function inputNumbers (array) {
+function inputNumbers () {
+  let array = [];
   for (let i = 0; i < 5; i++) {
-    const inputNum = parseInt(prompt('inserisci i numeri in fila'))
-    array.push(inputNum)
+    const inputNum = parseInt(prompt('inserisci i numeri in fila'));
+    array.push(inputNum);
   }
+  return array;
 }
 
-function getRandom (array, print) {
+function getRandom (array) {
   for (let i = 0; i < 5; i++) {
-    const random = getRndInteger(1, 9)
-    array.push(random)
+    const random = getRndInteger(1, 9);
+    array.push(random);
   }
-  print.innerText = numeriCasuali
+  numberBox.innerText = array;
+  return array;
+}
+
+function checkNumbers (numeriInseriti, numeriCasuali) {
+  let numeriIndovinati = [];
+  for (let i = 0; i < numeriInseriti.length; i++) {
+    if (numeriInseriti[i] == numeriCasuali[i]) {
+      numeriIndovinati.push(i + 1);
+    }
+  }
+  return numeriIndovinati;
 }
 
 function getRndInteger (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
